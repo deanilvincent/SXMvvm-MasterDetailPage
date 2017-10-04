441b6dd47abccc5a3ced1255eea48cd3079f91dc
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2017-10-04 23:11:45 +0800

    Add project files.

---
+++ SXMvvmMasterDetailPage/SXMvvmMasterDetailPage/ViewModels/MasterDetailPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SXMvvmMasterDetailPage.ViewModels
+{
+    public class MasterDetailPageViewModel
+    {
+        public MasterDetailPageViewModel()
+        {
+
+        }
+    }
+}
---
+++ SXMvvmMasterDetailPage/SXMvvmMasterDetailPage/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using SXMvvmMasterDetailPage.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SXMvvmMasterDetailPage
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(HomePage)));
+        }
+    }
+}
