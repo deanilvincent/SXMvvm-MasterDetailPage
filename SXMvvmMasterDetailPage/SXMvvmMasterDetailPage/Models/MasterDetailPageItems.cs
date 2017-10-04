06577eb121ffb150c0dffea5db987f9f44c266ed
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2017-10-04 23:11:45 +0800

    Add project files.

---
+++ SXMvvmMasterDetailPage/SXMvvmMasterDetailPage/Models/MasterDetailPageItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SXMvvmMasterDetailPage.Models
+{
+    public class MasterDetailPageItems
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type TypeOf { get; set; }
+    }
+}
